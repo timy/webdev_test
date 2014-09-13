b8247b03a82ee0a2aa0fd1e901ac1b907b161d90
Tian-Min Yan <timy.tech@gmail.com>
2014-09-13 21:21:45 +0200

    Add view and jade template. Render web page with template engine

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.server = require('http').createServer(app);
